d93d9262863cedffefc6eb9aadf37d2b831a90af
914629 <914629@U019311.adm.ehu.es>
2020-03-09 19:42:00 +0100

    Get pusheado

---
+++ Dragamina/src/Eredua/Mina.java
@@ -0,0 +1,26 @@
+package Eredua;
+
+public class Mina {
+	
+	private int errenkada;
+	private int zutabe;
+	
+	public Mina(int pZutabe, int pErrenkada) {
+		
+		this.errenkada=pErrenkada;
+		this.zutabe=pZutabe;
+		
+	}
+	
+	public int getZutabea() {
+		
+		return this.zutabe;
+		
+	}
+	
+	public int getErrenkada() {
+		
+		return this.errenkada;
+	}
+
+}
