fd3dd3722c560ac24f85e066b45700ca37b4b729
GySyX <GySyX@192.168.0.20>
2020-03-14 14:29:53 +0100

    BOOOOOOOOOOm

--- Dragamina/src/Eredua/Matrizea.java
+++ Dragamina/src/Eredua/Matrizea.java
@@ -3,7 +3,16 @@ package Eredua;
 public class Matrizea {
     private Kasilla[][] matrize;
 
-    public Matrizea(int pErrenkadaKop,int pZutabeKop){
-        
+    public Matrizea(int pErrenkadaKop,int pZutabeKop, int pZailtasuna){
+    	
+    	this.matrize = new Kasilla[pErrenkadaKop][pZutabeKop]; 
+    	this.minakEsleitu(pZailtasuna);
+    }
+    
+    
+    private void minakEsleitu(int pZailtasuna){
+    	
+    	int minaKop = pZailtasuna*this.matrize[0].length;
+    	
     }
 }
